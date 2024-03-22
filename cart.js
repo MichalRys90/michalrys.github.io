b5d55d9184ce212aca5b38a7c6ac58995874fc6f
MichalRys90 <mrys@tgo.pl>
2024-03-23 00:48:36 +0100

    Adding library and some cart

---
+++ cart.js
@@ -0,0 +1,107 @@
+var CART_API = "http://localhost:8080/v1";
+var text = "";
+var textKonto = "<input type='text' id = usna class = 'log' placeholder = 'Username'><br><input type='text' id = fina class = 'log' placeholder = 'First name'><br><input type='text' id=lana class = 'log' placeholder = 'Last name'><br><input type='text' id=pass1 class = 'log' placeholder = 'Password'><input type='text' id=pass2 class = 'log' placeholder = 'Repeat password'><br><button id='crac' class='creacou'> Create Acoount! </button>";
+var temp = "<p id='tempor'><br><br><br><br><br>The account has been created</p>";
+var pizzeria = "<div id='pizzeria'><div id='stronaglowna' class='restaurant'>Strona główna</div><div id='pizzamenu' class='restaurant'>Menu</div><div id='pizzakontakt'class='restaurant'>Kontakt</div><div style='clear:both;'></div></div>";
+var token;
+
+
+cart.addEventListener("click", function() {
+	con.innerHTML = cartText;
+});
+
+	document.body.addEventListener("click", function(e) {
+	
+		if(e.target.id == "create") {
+			    con.innerHTML = textKonto;
+		}
+	});
+	
+	
+	async function createAccount(username, firstname, lasname, password1, password2) {
+	if (password1 == password2) {
+		try {
+			const response = await fetch(CART_API + "/users/CreateUser", {
+				method: 'POST',
+				headers: {
+					'Accept': 'application/json',
+					'Content-Type': 'application/json'
+				},
+				body: JSON.stringify({
+					username: username,
+					firstName: firstname,
+					lastName: lasname,
+					password: password1
+				})
+			});
+
+			const responseData = await response.json();
+
+			if (responseData) {
+				setTimeout(() => {
+					con.innerHTML = cartText;
+				}, "2000");
+				console.log("Dodano użytkownika");
+				con.innerHTML = temp;
+			} else {
+				throw new Error('Coś poszło nie tak');
+			}
+		} catch (error) {
+			console.error('Wystąpił błąd:', error);
+			// Obsługa błędu (opcjonalna)
+		} 
+	} else {
+		con.innerHTML = "Podane hasła różnią się od siebie, spróbuj ponownie<br><br><br>" + textKonto;
+		}
+} 
+
+	document.body.addEventListener("click", function(e) {
+	
+		if(e.target.id == "crac") {
+			    var userna = usna.value;
+			    var firstna = fina.value;
+			    var lastna = lana.value;
+			    var pas1 = pass1.value;
+			    var pas2 = pass2.value;
+				createAccount(userna, firstna, lastna, pas1, pas2)
+		}
+	});
+	
+	
+	async function Login(username, password) {
+
+    try {
+        const response = await fetch(CART_API + "/users/login?username=" + username + "&password=" + password, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+
+        if (!response.ok) {
+            throw new Error('Odpowiedź nie prawidłowa');
+        }
+
+        const responseData = await response.text();
+        console.log('Response data:', responseData);
+		token = responseData;
+        con.innerHTML = pizzeria;
+
+		const container = document.querySelector('#content');
+		container.style.backgroundColor = '#E15252';
+		
+    } catch (error) {
+        console.error('Wystąpił problem z operacją fetch:', error.message);
+        // Tutaj możesz obsłużyć błąd
+    }
+}
+
+
+	document.body.addEventListener("click", function(e) {
+	
+		if(e.target.id == "logon") {
+			    var userna = login.value;
+			    var pass = passto.value;
+				Login(userna, pass)
+		}
+	});
---
+++ library.js
@@ -0,0 +1,296 @@
+var LIBRARY_API = "http://localhost:8080";
+var allBooks;
+var title;
+const table1 = [];
+var nickNameToHire;
+
+library.addEventListener("click", function() {
+	con.innerHTML = libraryTekst;
+});
+
+var text = "";
+
+async function getAllBooks() {
+  const response = await fetch(LIBRARY_API + "/book");
+  
+  allBooks = await response.json();
+  var ul = document.createElement("ul");
+  for (a of allBooks) {
+	  var li =document.createElement("li");
+	  const response1 = await fetch(LIBRARY_API + "/title/" + a.titleId);
+	  title = await response1.json();
+		text = text + "Title - " + title.title + "<br><br>Author - " + title.author + "<br><br>Publication year - " + title.publicationYear + "<br><br>Status - " + a.status + "<br><br>";
+		li.innerHTML = text;
+		ul.appendChild(li);
+		text = "";
+  }
+  
+  con.innerHTML = libraryTekst;
+  con.innerHTML += "<br>";
+  con.innerHTML += "<br>";
+  con.innerHTML += "<br>";
+  con.appendChild(ul);
+  
+  return text;
+}
+
+async function getFreeBooks() {
+  const response = await fetch(LIBRARY_API + "/book");
+  
+  allBooks = await response.json();
+  var ul = document.createElement("ul");
+  var i = 0;
+  for (let a of allBooks) {
+	  var li =document.createElement("li");
+	  var inputs = document.createElement("input");
+	  inputs.setAttribute('type', 'text');
+	  inputs.placeholder="Type nickname to rent a book";
+	  var button = document.createElement("button");
+	  button.setAttribute('class', 'hire');
+	  button.textContent = "Hire!";
+	  const response1 = await fetch(LIBRARY_API + "/title/" + a.titleId);
+	  title = await response1.json();
+	  if (a.status == "AVAILABLE") {
+		i++;
+		table1.push(a.id);
+		button.setAttribute('id', 'button' + i);
+		inputs.setAttribute('id', 'input' +i);
+		text = text + "Title - " + title.title + "<br><br>Author - " + title.author + "<br><br>Publication year - " + title.publicationYear + "<br><br>Status - " + a.status + "<br><br>";
+		li.innerHTML = text;
+		li.appendChild(inputs);
+		li.appendChild(button);
+		ul.appendChild(li);
+		text = "";
+		
+            button.addEventListener('click', function() {
+                var inputId = this.id.replace('button', 'input');
+                var input = document.getElementById(inputId);
+                if (input) {
+                    var nickname = input.value;
+                    hire(a.id, nickname);
+					setTimeout(getFreeBooks, 1000);
+                } else {
+                    console.error('Input not found!');
+                }
+            });
+	  }
+  }
+  
+  con.innerHTML = libraryTekst;
+  con.innerHTML += "<br>";
+  con.innerHTML += "<br>";
+  con.innerHTML += "<br>";
+  con.appendChild(ul);
+  
+  return text;
+}
+
+async function hire(booksId, readerNickname) {
+	const response = await fetch(LIBRARY_API + "/reader/readId?nickname="  + readerNickname);
+	id = await response.json();
+	const response1 = await fetch(LIBRARY_API + "/hire/rent?bookId="+booksId+"&readerId="+id, {
+  method: 'POST',
+});
+};
+
+	document.body.addEventListener("click", function(e) {
+		
+		if(e.target.id == "allBooks") {
+				getAllBooks();
+		}
+});
+
+	document.body.addEventListener("click", function(e) {
+		
+		if(e.target.id == "freebooks") {
+				getFreeBooks();
+		}
+});
+
+	document.querySelectorAll('hire').forEach((el, i) => {
+		el.addEventListener('click', () => alert(`cliked ${i+1}`))
+	})
+
+
+async function addReader(name, lastname, nickname) {
+			const response = await fetch(LIBRARY_API + "/reader", {
+				method: 'POST',
+			headers: {
+		'Accept': 'application/json',
+		'Content-Type': 'application/json'
+	},
+body: `{
+   "name" : ${name},
+   "lastName" : ${lastname},
+   "nickName" : ${nickname}
+  }`,
+});
+  nameu.value="";
+  lastnameu.value="";
+  nicknameu.value="";
+}
+
+	document.body.addEventListener("click", function(e) {
+	
+		if(e.target.id == "reader") {
+			    var nam = '"' + nameu.value + '"';
+			    var lastnam = '"' + lastnameu.value + '"';
+			    var nicknam = '"' + nicknameu.value + '"';
+				addReader(nam, lastnam, nicknam);
+		}
+	});
+	
+async function getUserId(nickname) {
+	const response = await fetch(LIBRARY_API + "/reader/readId?nickname="  + nickname);
+	var id = await response.json();
+	return id;
+}
+
+async function getAllHiresForUser(id) {
+	    const response = await fetch(LIBRARY_API + "/hire/user?readerId=" + id);
+		const list = await response.json();
+		return list;
+};
+
+		
+		async function getUserHires() {
+			var ul = document.createElement("ul");
+			var nickname = returnhire.value;
+			if (nickname != "") {
+				nickNameToHire = nickname;
+			}
+			const response = await fetch(LIBRARY_API + "/reader/readId?nickname="  + nickNameToHire);
+	        var id = await response.json();
+			const response1 = await fetch(LIBRARY_API + "/hire/user?readerId=" + id);
+			let list = await response1.json();
+			var i = 0;
+			for (li of list) {
+				var x = li.id;
+				const response = await fetch(LIBRARY_API + "/book/" +li.bookId);
+				book = await response.json();
+				if (book.status == "RENTED" && li.returned == null) {
+				i++;
+				var li =document.createElement("li");
+				var button = document.createElement("button");
+				button.setAttribute('class', 'hire');
+				button.textContent = "Return book!";
+				const response2 = await fetch(LIBRARY_API + "/title/" + book.titleId);
+				title = await response2.json();
+				ul.appendChild(li);
+				button.setAttribute('id', x);
+				text = text + "Title - " + title.title + "<br><br>Author - " + title.author + "<br><br>Publication year - " + title.publicationYear + "<br><br>Status - " + book.status + "<br><br>";
+				li.innerHTML = text;
+				li.appendChild(button);
+				ul.appendChild(li);
+				text = "";
+				
+				button.addEventListener('click', function() {
+					var buttonId = this.id.replace('button');
+					var button = document.getElementById(buttonId);
+					if (button) {
+                    var buttonIds = button.id;
+					ret(buttonIds);
+					setTimeout(getUserHires, 1000);
+                } else {
+                    console.error('Input not found!');
+                }
+				});
+			}
+		}
+		con.innerHTML = libraryTekst;
+		con.innerHTML += "<br>";
+		con.innerHTML += "<br>";
+		con.innerHTML += "<br>";
+		con.appendChild(ul);
+	}
+		
+	    async function ret(li) {
+			const response2 = await fetch(LIBRARY_API + "/hire/return?hireId=" + li, {
+				method: 'PUT'
+				});
+}
+		
+	document.body.addEventListener("click", function(e) {
+		if(e.target.id == "showrent") {
+			getUserHires();
+		}
+	});
+	
+		 async function howManyAvailable(li) {
+			const response2 = await fetch(LIBRARY_API + "/book/howManyFree?title=" + li)
+			const howManyFree = await response2.json();
+			con.innerHTML = libraryTekst;
+			con.innerHTML += "<br>";
+			con.innerHTML += "<br>";
+			con.innerHTML += "Free books with title " + li + " is " + howManyFree;
+		return howManyFree;
+}
+
+	document.body.addEventListener("click", function(e) {
+		if(e.target.id == "showHowManyFree") {
+			var go = howManyFree.value;
+			howManyAvailable(go);
+		}
+	});
+	
+async function addTitle(title, author, publicationYear) {
+    try {
+        const response = await fetch(LIBRARY_API + "/title", {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                title: title,
+                author: author,
+                publicationYear: publicationYear
+            })
+        });
+
+        const responseData = await response.json();
+        const id = responseData.id;
+
+        if (id) {
+            console.log("ID dodanego tytułu:", id);
+            addBook(id);
+        } else {
+            throw new Error('Odpowiedź serwera nie zawiera ID.');
+        }
+
+    } catch (error) {
+        console.error('Wystąpił błąd:', error);
+        // Obsługa błędu (opcjonalna)
+    }
+	titleBook.value = "";
+	authorBook.value = "";
+	publicationYearBook.value = "";
+}
+
+   async function addBook(titleId) {
+			const response = await fetch(LIBRARY_API + "/book", {
+			method: 'POST',
+			headers: {
+		'Accept': 'application/json',
+		'Content-Type': 'application/json'
+	},
+	body: `{
+		"titleId" : ${titleId}
+		}`,
+	});
+}
+
+	document.body.addEventListener("click", function(e) {
+	
+		if(e.target.id == "addBook") {
+			    var title1 = titleBook.value;
+			    var author1 = authorBook.value;
+			    var publicationYear1 = publicationYearBook.value;
+				addTitle(title1, author1, publicationYear1);
+		}
+	});
+
+    
+			
+			
+		
